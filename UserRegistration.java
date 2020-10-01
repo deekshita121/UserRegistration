2e5bdfec0cde67e03587be2c29861fed2210e1f2
Dee121-ala <deekshitaalavala@gmail.com>
2020-10-01 22:08:20 +0530

    [deekshita] Add : Added usecase2

--- UserRegistration.java
+++ UserRegistration.java
@@ -5,11 +5,11 @@ public class UserRegistration
      public static void main(String args[])
      {
           Scanner sc = new Scanner(System.in);
-          System.out.println("Enter the first name of user");
-          String firstName = sc.nextLine();
+          System.out.println("Enter the last name of user");
+          String lastName = sc.nextLine();
           String check = "[A-Z]{1}[a-z]{2,}";
           Pattern p = Pattern.compile(check);
-          Matcher m = p.matcher(firstName);
+          Matcher m = p.matcher(lastName);
           if(m.matches())
           System.out.println("Valid");
           else
