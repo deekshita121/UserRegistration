0c6ae20a075778484e6ae3d1c418fd43423b4839
Dee121-ala <deekshitaalavala@gmail.com>
2020-10-02 12:56:30 +0530

    [deekshita]Add: Added usecase1

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,7 +1,18 @@
+import java.util.*;
+import java.util.regex.*;
 public class UserRegistration
 {
-public static void main(String args[])
-{
-System.out.println("Welcome");
-}
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first name of user");
+        String firstName = sc.nextLine();
+        String check = "[A-Z]{1}[a-z]{2,}";
+        Pattern p = Pattern.compile(check);
+        Matcher m = p.matcher(firstName);
+        if(m.matches())
+        System.out.println("Valid");
+        else
+        System.out.println("Invalid");
+    }
 }
