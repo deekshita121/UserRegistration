314ebb77509e0ed87df1b8ab48f195c2db62e084
Dee121-ala <deekshitaalavala@gmail.com>
2020-10-02 15:36:24 +0530

    [deekshita]Add : Added usecase4

--- UserRegistration.java
+++ UserRegistration.java
@@ -5,10 +5,10 @@ public class UserRegistration
      public static void main(String args[])
      {
           Scanner sc = new Scanner(System.in);
-          System.out.println("Enter the emailId of user");
-          String emailId = sc.nextLine();
-          String check = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
-          if(emailId.matches(check))
+          System.out.println("Enter the phone num of user with country code");
+          String phoneNum = sc.nextLine();
+          String check = "[91]{2}\\s[6-9]{1}[0-9]{9}$";
+          if(phoneNum.matches(check))
           System.out.println("Valid");
           else
           System.out.println("Invalid");
