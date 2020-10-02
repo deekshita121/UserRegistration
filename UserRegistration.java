a85a14a875f71149cbc69640a6b4f70cf8cdc0f8
Dee121-ala <deekshitaalavala@gmail.com>
2020-10-02 20:53:32 +0530

    [deekshita] Add : Added usecase9

--- UserRegistration.java
+++ UserRegistration.java
@@ -5,11 +5,11 @@ public class UserRegistration
      public static void main(String args[])
      {
           Scanner sc = new Scanner(System.in);
-          System.out.println("Enter the password of user");
-          String password = sc.nextLine();
-          String check = "(?=.*[A-Z])(?=.*[0-9])(?=.*[!@%$&*_.?])[A-Za-z0-9!@$%&*_?.]{8,}$";
+          System.out.println("Enter the emailId of user");
+          String emailId = sc.nextLine();
+          String check = "^[a-zA-Z]+[a-zA-Z0-9+_,-]+.{0,1}+[a-zA-Z0-9]+@[a-zA-Z0-9]+.[a-zA-Z0-9,]+.{0,1}+[0-9]{0,1}+[a-zA-Z,]{2,}+$";
           Pattern p = Pattern.compile(check);
-          Matcher m = p.matcher(password);
+          Matcher m = p.matcher(emailId);
           if(m.matches())
           System.out.println("Valid");
           else
